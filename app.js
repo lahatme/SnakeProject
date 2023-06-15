ef71e05dcdb40e0169e678fc71192c0859b710a2
lahatme <30632811+lahatme@users.noreply.github.com>
2023-06-15 23:52:02 +0300

    Creating-the-board-in-JS

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ const blockSize = 25;
 var board;
 var context;
 
-window.onload = function CreateBoard(row){
-    console.log("hii");
-    const myCanvas = document.getElementById("myCanvas");
-    for(let i =0 ; i<10;i++){
+window.onload = function Onload(){
+ CreateBoard(10);
+} 
+
+
+
+function CreateBoard(size){
+    const body = document.getElementById("body");
+    body.style.justifyContent="center";
+
+    const board = document.createElement("div");
+    body.appendChild(board);
+    board.style.display = "flex";
+    board.style.flexDirection= "column";
+    board.style.gap= "5px";
+    board.style.position="absolute";
+    board.style.top="31%";
+    board.style.left= "46%";
+    for(let i =0; i<size; i++){
         const row = document.createElement("div")
-        for(let j =0 ; j<10;j++){
+        row.style.display = "flex";
+        row.style.flexDirection= "row";
+        row.style.gap= "5px";
+        for(let j =0; j<size; j++){
             const cell = document.createElement("div")
+            cell.style.height = "20px";
+            cell.style.width = "20px";
+            cell.style.background = "red"
             row.appendChild(cell)
         }
-        myCanvas.appendChild(row);
+        board.appendChild(row);
     }
    
 }
 
-//CreateBoard(10);
